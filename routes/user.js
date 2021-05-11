328911740f8230116746ce850204fd599fd30a7e
Phinahas Philip <phinahasphilip2000@gmail.com>
2021-05-11 23:03:34 +0530

    Index page and home page created. In layout page w3.css added

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('users/index');
 });
 
 //Page to create a new user
 router.get('/signup',(req,res)=>{
-  res.render('user-signup')
+  res.render('users/user-signup')
 })
 
 //loginPage
 router.get('/login',(req,res)=>{
-  res.render('user-login')
+  res.render('users/user-login')
+})
+
+//bloger home page for viewing blogs
+router.get('/home',(req,res)=>{
+  res.render('users/user-home')
 })
 module.exports = router;
